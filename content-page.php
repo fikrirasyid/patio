2765080e493ee8bcb4db880aed64a0cc5d3a78df
Fikri Rasyid <fikrirasyid@gmail.com>
2015-03-25 10:33:10 +0700

    Fixing styling detail on comment section

--- content-page.php
+++ content-page.php
@@ -1,27 +1,33 @@
 <?php
 /**
- * The template used for displaying page content in page.php
- *
  * @package Patio
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<?php if ( has_post_thumbnail() ): ?>
+	<div class="entry-featured-image">
+		<?php the_post_thumbnail( 'full' ); ?>
+	</div><!-- .entry-featured-image -->
+	<?php endif; ?>
+	
+	<div class="hentry-wrap">
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'patio' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'patio' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'patio' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+		<footer class="entry-footer">
+			<?php edit_post_link( __( 'Edit', 'patio' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-footer -->
+	</div><!-- .hentry-wrap -->
 </article><!-- #post-## -->
--- page.php
+++ page.php
@@ -1,34 +1,30 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * The template for displaying all single posts.
  *
  * @package Patio
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<?php while ( have_posts() ) : the_post(); ?>
+	
+		<div class="wrap">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">	
+					<?php get_template_part( 'content', 'page' ); ?>
+				</main><!-- #main -->
+			</div><!-- #primary -->			
+		</div><!-- .wrap -->
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+	<?php endwhile; // end of the loop. ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php get_footer(); ?>
