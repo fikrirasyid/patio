d9b2ef3c47b4d4be6de19a6ac67054a40a1356e7
Fikri Rasyid <fikrirasyid@gmail.com>
2015-03-25 10:33:30 +0700

    Fixing page's template
    Making it follows single page template structure

--- single.php
+++ single.php
@@ -7,35 +7,35 @@
 
 get_header(); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-		
-			<div class="wrap">
-				<div id="primary" class="content-area">
-					<main id="main" class="site-main" role="main">	
-						<?php get_template_part( 'content', 'single' ); ?>
-				
-						<?php
-							the_post_navigation( array(
-								'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'See Next', 'patio' ) . '</span> ' .
-									'<span class="screen-reader-text">' . __( 'Next post:', 'patio' ) . '</span> ' .
-									'<span class="post-title">%title</span>',
-								'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previously', 'patio' ) . '</span> ' .
-									'<span class="screen-reader-text">' . __( 'Previous post:', 'patio' ) . '</span> ' .
-									'<span class="post-title">%title</span>',
-							) );
-						?>
-					</main><!-- #main -->
-				</div><!-- #primary -->			
-			</div><!-- .wrap -->
+	<?php while ( have_posts() ) : the_post(); ?>
+	
+		<div class="wrap">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">	
+					<?php get_template_part( 'content', 'single' ); ?>
+			
+					<?php
+						the_post_navigation( array(
+							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'See Next', 'patio' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Next post:', 'patio' ) . '</span> ' .
+								'<span class="post-title">%title</span>',
+							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previously', 'patio' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Previous post:', 'patio' ) . '</span> ' .
+								'<span class="post-title">%title</span>',
+						) );
+					?>
+				</main><!-- #main -->
+			</div><!-- #primary -->			
+		</div><!-- .wrap -->
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+		<?php
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		?>
 
-		<?php endwhile; // end of the loop. ?>
+	<?php endwhile; // end of the loop. ?>
 
 
 <?php get_footer(); ?>
