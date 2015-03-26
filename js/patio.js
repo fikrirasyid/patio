74837cde1772a299d4e06486f41a661c9a0429de
Fikri Rasyid <fikrirasyid@gmail.com>
2015-03-26 11:15:36 +0700

    Styling respond link

--- js/patio.js
+++ js/patio.js
@@ -2,16 +2,18 @@ jQuery(document).ready(function($){
 	/**
 	* Auto adjust post thumbnail
 	*/
-	$('.entry-thumbnail').each(function(){
-		var thumb 			= $(this);
-		var thumb_height 	= thumb.outerHeight();
-		var img 			= thumb.find( 'img' );
-		var img_width 		= img.outerWidth();
-		var img_height 		= img.outerHeight();
+	$(window).load(function(){
+		$('.entry-thumbnail').each(function(){
+			var thumb 			= $(this);
+			var thumb_height 	= thumb.outerHeight();
+			var img 			= thumb.find( 'img' );
+			var img_width 		= img.outerWidth();
+			var img_height 		= img.outerHeight();
 
-		if( thumb_height > img_height ){
-			thumb.addClass( 'height-adjustment' );
-		}
+			if( thumb_height > img_height ){
+				thumb.addClass( 'height-adjustment' );
+			}
+		});
 	});
 
 	/**
