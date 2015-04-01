08e46d416c49c6842962777c0a143ebb234bb170
Fikri Rasyid <fikrirasyid@gmail.com>
2015-04-01 08:49:22 +0700

    Making content-search == content template

--- content-search.php
+++ content-search.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * The template part for displaying results in search pages.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
  * @package Patio
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<a href="<?php the_permalink(); ?>" title="<?php printf( __( 'Permanent link to %s', 'patio' ), esc_attr( get_the_title() ) ); ?>" class="entry-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</a>
+	<?php endif; ?>
+
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
@@ -19,11 +21,9 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<?php if( ! has_post_thumbnail() ): ?>
+	<div class="entry-excerpt">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php patio_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</div><!-- .entry-content -->		
+	<?php endif; ?>
 </article><!-- #post-## -->
