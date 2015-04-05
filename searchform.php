891cefd0ae18415e36517588f10ca0c122d26659
Fikri Rasyid <fikrirasyid@gmail.com>
2015-04-05 11:38:18 +0700

    Use good ol dots

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'patio' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Type keyword and hit enter to search…', 'placeholder', 'patio' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'patio' ) ?>" />
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Type keyword and hit enter to search...', 'placeholder', 'patio' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'patio' ) ?>" />
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'patio' ) ?>" />
 </form>
